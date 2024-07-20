54f6c90f646c1a9c15986a836d4146a907770140
behrooz bozorgchamy <behroozbc@outlook.com>
2024-07-20 15:01:04 +0200

    NOT TEST changes to pix2pix

---
+++ data/weather_dataset.py
@@ -0,0 +1,47 @@
+from data.base_dataset import BaseDataset, get_transform
+from data.image_folder import make_dataset
+from PIL import Image
+import random
+class WeatherDataset(BaseDataset):
+    def __init__(self,opt):
+        BaseDataset.__init__(self, opt)
+        self.dir_camera = os.path.join(opt.dataroot, opt.phase + '_camera')  # create a path '/path/to/data/train_camera'
+        self.dir_cp = os.path.join(opt.dataroot, opt.phase + '_cp')  # create a path '/path/to/data/train_cp'
+        self.dir_weather = os.path.join(opt.dataroot, opt.phase + '_weather')  # create a path '/path/to/data/train_weather'
+        self.camera_paths = sorted(make_dataset(self.dir_camera, opt.max_dataset_size))   # load images from '/path/to/data/train_camera'
+        self.cp_paths = sorted(make_dataset(self.dir_cp, opt.max_dataset_size))    # load images from '/path/to/data/train_cp'
+        self.weather_paths = sorted(make_dataset(self.dir_weather, opt.max_dataset_size))    # load images from '/path/to/data/train_weather'
+        self.camera_size = len(self.camera_paths)  # get the size of dataset camera
+        self.cp_size = len(self.cp_paths)  # get the size of dataset cp
+        self.weather_size = len(self.cp_paths)  # get the size of dataset cp
+        self.weather_paths = len(self.weather_paths)  # get the size of dataset weather
+        self.camera_transport=get_transform(opt)
+        self.weather_trasport=get_transform(opt,grayscale=True)
+        self.cp_transport=get_transform(opt,grayscale=True)
+    def __getitem__(self, index):
+        cameraPath = self.camera_paths[index % self.camera_size]  # make sure index is within then range
+        if self.opt.serial_batches:   # make sure index is within then range
+            indexCp = index % self.cp_size
+            indexWeather= index % self.weather_size
+        else:   # randomize the index for domain B to avoid fixed pairs.
+            indexCp = random.randint(0, self.cp_size - 1)
+            indexWeather = random.randint(0, self.weather_size - 1)
+        cpPath = self.cp_paths[indexCp]
+        weatherPath = self.weather_paths[indexWeather]
+        
+        camera_img = Image.open(cameraPath).convert('RGB')
+        cp_img = Image.open(cpPath).convert('L')
+        weather_img=Image.open(weatherPath).convert('L')
+        
+        # apply image transformation
+        camera = self.camera_transport(camera_img)
+        cp = self.cp_transport(cp_img)
+        weather=self.weather_trasport(weather_img)
+        return {'camera': camera, 'cp': cp,'weather':weather, 'camera_paths': cameraPath, 'cp_paths': cpPath,'weather_paths':weatherPath}
+    def __len__(self):
+        """Return the total number of images in the dataset.
+
+        As we have two datasets with potentially different number of images,
+        we take a maximum of
+        """
+        return max(self.weather_size,max(self.camera_size,self.cp_size))
--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -78,10 +78,18 @@ class Pix2PixModel(BaseModel):
 
         The option 'direction' can be used to swap images in domain A and domain B.
         """
-        AtoB = self.opt.direction == 'AtoB'
-        self.real_A = input['A' if AtoB else 'B'].to(self.device)
-        self.real_B = input['B' if AtoB else 'A'].to(self.device)
-        self.image_paths = input['A_paths' if AtoB else 'B_paths']
+        if self.opt.input_number==2:
+            AtoB = self.opt.direction == 'AtoB'
+            self.real_A = input['A' if AtoB else 'B'].to(self.device)
+            self.real_B = input['B' if AtoB else 'A'].to(self.device)
+            self.image_paths = input['A_paths' if AtoB else 'B_paths']
+        else:
+            self.real_B =input['cp'].to(self.device)
+            weatherLabel=input['weather'].to(self.device)
+            camera=input['camera'].to(self.device)
+            
+            self.real_A=torch.cat((camera,weatherLabel),1)
+            
 
     def forward(self):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
